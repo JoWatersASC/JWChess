2f89b08dc31fc7016d9e503e34e818696a616d50
JoWatersASC <joshua.waters365@gmail.com>
2024-08-22 10:50:04 -0400

    Updated init for events

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,12 +5,14 @@ namespace jwchess {
 
 	int Init() {
 		SDL_Init(SDL_INIT_EVERYTHING);
+        g = Grid::grid();
 
-        window = SDL_CreateWindow("JW Chess", 200, 60, 700, 700);
-        DisplayManager::window = window;
+        window = SDL_CreateWindow("JW Chess", 200, 60, 700, 700, 0);
 
-        if (!DisplayManager::Init()) exit(-1);
+        if (!Events::Init(window, &g)) exit(-1);
+        if (!DisplayManager::Init(window)) exit(-1);
         DisplayManager::s_grid = &g;
+        DisplayManager::window = window;
 
         Grid::move m(g["a2"], g["h5"]);
         m.act();
@@ -26,6 +28,7 @@ namespace jwchess {
         SDL_Event e;
         while (running) {
             while (SDL_PollEvent(&e)) {
+                Events::Handle(e);
 
                 if (e.type == SDL_QUIT) {
                     running = false;
